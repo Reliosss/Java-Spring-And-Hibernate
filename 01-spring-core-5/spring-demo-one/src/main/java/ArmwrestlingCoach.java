a541b25588ed7110f826aa17a218c0d14606d10a
Reliosss <drohomireckiadrian@gmail.com>
2020-07-20 23:05:57 +0200

    types of injection: constructor, setter and literal values

---
+++ 01-spring-core-5/spring-demo-one/src/main/java/ArmwrestlingCoach.java
@@ -0,0 +1,12 @@
+public class ArmwrestlingCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout(){
+        return "Make 20 push ups!";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return null;
+    }
+}
--- 01-spring-core-5/spring-demo-one/src/main/java/TrackCoach.java
+++ 01-spring-core-5/spring-demo-one/src/main/java/TrackCoach.java
@@ -1,7 +1,22 @@
 public class TrackCoach implements Coach {
 
+    private FortuneService fortuneService;
+
+    public TrackCoach(){
+
+    }
+
+    public TrackCoach(FortuneService fortuneService){
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout(){
         return "Run a hard 5 miles!";
+    }
+
+    @Override
+    public String getDailyFortune(){
+        return "This is your day!";
     }
 }
